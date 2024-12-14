c241cafad42a7b8afd9863e4c4a6255a20b59470
Marcos Patrick <marcospatrick039474@gmail.com>
2024-12-14 13:54:51 -0300

    modifying the UI theme files

--- app/src/main/java/com/nearby/offers/MainActivity.kt
+++ app/src/main/java/com/nearby/offers/MainActivity.kt
@@ -11,14 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.nearby.offers.ui.theme.NearbyOffersandroidTheme
+import com.nearby.offers.ui.theme.NearbyTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge();
         setContent {
-            NearbyOffersandroidTheme {
+            NearbyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +43,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    NearbyOffersandroidTheme {
+    NearbyTheme {
         Greeting("Android")
     }
 }
