17240093e4d4c7cfb4b2431e3e4a51c1f0ecab26
Marcos Patrick <marcospatrick039474@gmail.com>
2025-08-18 17:33:44 -0300

    continuando comment

--- app/src/main/java/com/nearby/offers/model/NearbyCategory.kt
+++ app/src/main/java/com/nearby/offers/model/NearbyCategory.kt
@@ -6,6 +6,7 @@ data class NearbyCategory(
     val id :String,
     val name: String
 ){
-    @get:DrawableRes // simboliza que o retorno de Get representa um Id de resource do projeto
+    @get:DrawableRes // simboliza que o retorno de Get representa um Id de resource do
+    // 'id resource' se trata do recurso q se encontra em 'app/src/main/res'
     val icon: Int? get() = NearbyCategoryFilterChipView.fromDescription( descriptionToBeChecked = name )?.icon
 }
